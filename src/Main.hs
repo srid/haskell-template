d2956c03b77b13d986591424a2fd47619f44d812
Sridhar Ratnakumar <srid@srid.ca>
2021-10-12 09:37:00 -0400

    Use non-ascii in demo

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,6 @@ import Main.Utf8 (withUtf8)
 
 main :: IO ()
 main = do
+  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
   withUtf8 $ do
-    putStrLn "Hello World"
-
+    putStrLn "Hello 🌎"
