e4babff8267cc2804acedaad5ce92f105195cdef
Sridhar Ratnakumar <srid@srid.ca>
2022-03-25 14:27:56 -0400

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "haskell-template's description";
   inputs = {
     # To find a suitable nixpkgs hash with cache, pick one from https://status.nixos.org/
-    nixpkgs.url = "github:nixos/nixpkgs/1ec61dd4167f04be8d05c45780818826132eea0d";
+    nixpkgs.url = "github:nixos/nixpkgs/4d60081494259c0785f7e228518fee74e0792c1b";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.nixpkgs.follows = "nixpkgs";
     flake-compat.url = "github:edolstra/flake-compat";
@@ -25,6 +25,7 @@
           # Change GHC version here. To get the appropriate value, run:
           #   nix-env -f "<nixpkgs>" -qaP -A haskell.compiler
           hp = pkgs.haskellPackages; # pkgs.haskell.packages.ghc921;
+          haskellFormatter = "fourmoluStandardGhc8107"; # The formatter to use from inputs.lint-utils
 
           project = returnShellEnv:
             hp.developPackage {
@@ -60,12 +61,13 @@
 
           # Used by `nix run ...`
           apps = {
-            format = inputs.lint-utils.apps.${system}.fourmoluStandard8107;
+            # TODO: do cabal and nix as well.
+            format = inputs.lint-utils.apps.${system}.${haskellFormatter};
           };
 
           # Used by `nix flake check`
           checks = {
-            format-haskell = inputs.lint-utils.linters.${system}.fourmoluStandardGHC8107;
+            format-haskell = inputs.lint-utils.linters.${system}.${haskellFormatter};
             format-cabal = inputs.lint-utils.linters.${system}.cabal;
             format-nix = inputs.lint-utils.linters.${system}.nixpkgs-fmt;
           };
