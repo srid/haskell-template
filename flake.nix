1faa95db187bf7c1d357f5b5656ec095fca2894b
Sridhar Ratnakumar <srid@srid.ca>
2022-09-07 17:06:25 -0400

    Try to use horizon-platform for GHC 9.4

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs.follows = "nixpkgs";
-    haskell-flake.url = "github:srid/haskell-flake";
+    haskell-flake.url = "github:srid/haskell-flake/srid/break-developPackage";
     treefmt-flake.url = "github:srid/treefmt-flake";
     check-flake.url = "github:srid/check-flake";
+
+    horizon-platform.url = "git+https://gitlab.homotopic.tech/horizon/horizon-platform";
+    all-cabal-hashes.url = "github:commercialhaskell/all-cabal-hashes?ref=hackage";
+    all-cabal-hashes.flake = false;
   };
 
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
@@ -16,10 +20,27 @@
         inputs.haskell-flake.flakeModule
         inputs.treefmt-flake.flakeModule
         inputs.check-flake.flakeModule
+        # TODO: Is there an idiomatic way to specify custom overlays for `pkgs`
+        # passed by flake-parts (rather than hacking around it like we do by
+        # setting _module.args here)?
+        ({
+          perSystem = { inputs', system, ... }: {
+            _module.args.pkgs = import nixpkgs {
+              inherit system;
+              overlays = [
+                (final: prev: {
+                  all-cabal-hashes = inputs.all-cabal-hashes;
+                })
+              ];
+            };
+          };
+        })
       ];
-      perSystem = { self', config, pkgs, ... }: {
+      perSystem = { self', config, pkgs, system, ... }: {
         haskellProjects.default = {
           root = ./.;
+          haskellPackages = pkgs.haskell.packages.ghc942;
+          overrides = inputs.horizon-platform.overrides.${system}.ghc942;
           buildTools = hp: {
             inherit (pkgs)
               treefmt;
