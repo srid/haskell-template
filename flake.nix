5e3f0dfbff07bb6410f53ce7042bd4cccac42658
Sridhar Ratnakumar <srid@srid.ca>
2022-09-23 13:36:24 -0400

    Prototype

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     haskell-flake.url = "github:srid/haskell-flake";
-    treefmt-flake.url = "github:srid/treefmt-flake";
+    treefmt-flake.url = "github:srid/treefmt-flake/treefmt-nix";
+    treefmt-nix.url = "github:numtide/treefmt-nix"; # WHY?
     check-flake.url = "github:srid/check-flake";
   };
 
@@ -20,19 +21,37 @@
       perSystem = { self', config, pkgs, ... }: {
         haskellProjects.default = {
           root = ./.;
-          buildTools = hp: {
-            inherit (pkgs)
-              treefmt;
-          } // config.treefmt.formatters;
+          buildTools = hp:
+            let
+              treefmtConfig = (inputs.treefmt-nix.lib.evalModule pkgs config.treefmt).config;
+              treefmtTools = {
+                treefmt = treefmtConfig.build.wrapper;
+              } // builtins.mapAttrs (_: v: v.package) treefmtConfig.programs;
+            in
+            treefmtTools // {
+              # Your buildtools go here
+            };
           # overrides = self: super: {}
           enableHLSCheck = true;
         };
-        treefmt.formatters = {
-          inherit (pkgs)
-            nixpkgs-fmt;
-          inherit (pkgs.haskellPackages)
-            cabal-fmt
-            fourmolu;
+        treefmt = {
+          projectRootFile = "flake.nix";
+          package = pkgs.treefmt;
+          programs = {
+            nixpkgs-fmt.enable = true;
+            cabal-fmt.enable = true;
+            # FIXME
+            /* ormolu = {
+              enable = true;
+              package = pkgs.haskellPackages.fourmolu;
+            }; */
+          };
+          /* settings.formatter.ormolu = {
+            # command = pkgs.haskellPackages.fourmolu;
+            options = [
+              "--ghc-opt"
+              "-XImportQualifiedPost"
+            ]; */
         };
       };
     };
