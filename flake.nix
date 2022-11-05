d0f2e931654ca03693d4003432fc71c991db9e27
Sridhar Ratnakumar <srid@srid.ca>
2022-11-05 10:53:07 -0400

    Remove unnecessary follows

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     haskell-flake.url = "github:srid/haskell-flake";
     treefmt-flake.url = "github:srid/treefmt-flake";
   };
