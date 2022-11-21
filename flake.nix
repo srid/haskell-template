7114d997b18644e3bc1af92247868c3ad72ef7d2
Sridhar Ratnakumar <srid@srid.ca>
2022-11-21 08:37:06 -0500

    Revert last two nixpkgs lock updates

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "srid/haskell-template: Nix template for Haskell projects";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # https://github.com/srid/haskell-template/issues/64
+    nixpkgs.url = "github:nixos/nixpkgs/0cfb3c002b61807ca0bab3efe514476bdf2e5478";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
     treefmt-flake.url = "github:srid/treefmt-flake";
