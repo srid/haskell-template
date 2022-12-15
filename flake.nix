fec03ff1008c00a95ad2d4b35dff5b0255b6741b
Sridhar Ratnakumar <srid@srid.ca>
2022-12-15 12:27:44 -0500

    desc

--- flake.nix
+++ flake.nix
@@ -38,7 +38,9 @@
         devShells.default =
           let
             mkCommand = name: v:
-              v.package or pkgs.writeShellApplication { inherit name; text = v.command; };
+              (v.package or pkgs.writeShellApplication { inherit name; text = v.command; }).overrideAttrs (oa: {
+                meta.description = if v ? description then v.description else oa.meta.description or "No description";
+              });
             wrapCommands = spec:
               let commands = lib.mapAttrsToList mkCommand spec;
               in
@@ -51,7 +53,11 @@
                     echo
                     echo "  ${
                         lib.concatStringsSep "\n  " 
-                          (map (drv: builtins.baseNameOf (lib.getExe drv)) commands)
+                          (map (drv: 
+                            let name = builtins.baseNameOf (lib.getExe drv);
+                                desc = drv.meta.description;
+                            in name + " : " + desc
+                          ) commands)
                     }"
                   }
                   if [ "$*" == "" ] || [ "$*" == "-h" ] || [ "$*" == "--help" ]; then
@@ -70,22 +76,28 @@
             nativeBuildInputs = (oa.nativeBuildInputs or [ ]) ++ [
               (
                 (wrapCommands {
-                  # TODO: banner-grouping and meta.description
+                  # TODO: banner-grouping 
                   hoog = {
+                    description = "Start Hoogle server for project dependencies";
                     command = ''
                       echo http://127.0.0.1:8888
                       hoogle serve -p 8888 --local
                     '';
+                    category = "Dev Tools";
                   };
                   repl = {
+                    description = "Start the cabal repl";
                     command = ''
                       cabal repl
                     '';
+                    category = "Dev Tools";
                   };
                   run = {
+                    description = "Run the project with ghcid auto-recompile";
                     command = ''
                       ghcid -c "cabal repl exe:haskell-template" --warnings -T :main
                     '';
+                    category = "Primary";
                   };
                 }).overrideAttrs (_oa: {
                   meta.description = "Development scripts command";
