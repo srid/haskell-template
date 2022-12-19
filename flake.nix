b21f59e123a0fb498fea7d7a3f3b36d281bcdfc6
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2022-12-19 11:16:12 -0500

    Update flake.lock (#74)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/d591857e9d7dd9ddbfba0ea02b43b927c3c0f1fa' (2022-11-14)
      → 'github:hercules-ci/flake-parts/3f7172646953bf86dad5953bc45f0edae62ac445' (2022-12-18)
    • Updated input 'haskell-flake':
        'github:srid/haskell-flake/4fc511d93a55fedf815c1647ad146c26d7a2054e' (2022-11-11)
      → 'github:srid/haskell-flake/54334cfae9bbb73732bbb1437260017044f68d0b' (2022-12-19)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/a2d2f70b82ada0eadbcb1df2bca32d841a3c1bf1' (2022-12-04)
      → 'github:nixos/nixpkgs/bb31220cca6d044baa6dc2715b07497a2a7c4bc7' (2022-12-19)
    
    * Fix flake-parts warning
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   };
 
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit self; } {
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         inputs.haskell-flake.flakeModule
