5f9f15e959cf926d2c2ab4e99b82fcbc13e0f811
Mahdi Seyedan <mahdi.se@yahoo.com>
2024-01-29 04:20:09 +0330

    Nixify fourmolu.yaml

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     haskell-flake.url = "github:srid/haskell-flake";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    fourmolu-nix.url = "github:jedimahdi/fourmolu-nix";
   };
 
   outputs = inputs:
@@ -15,6 +16,7 @@
       imports = [
         inputs.haskell-flake.flakeModule
         inputs.treefmt-nix.flakeModule
+        inputs.fourmolu-nix.flakeModule
       ];
       perSystem = { self', system, lib, config, pkgs, ... }: {
         # Our only Haskell project. You can have multiple projects, but this template
@@ -49,6 +51,7 @@
           # Development shell configuration
           devShell = {
             hlsCheck.enable = false;
+            tools = hp: { fourmolu = config.fourmolu.wrapper; };
           };
 
           # What should haskell-flake add to flake outputs?
@@ -60,19 +63,24 @@
         treefmt.config = {
           projectRootFile = "flake.nix";
 
-          programs.ormolu.enable = true;
+          programs.fourmolu.enable = true;
           programs.nixpkgs-fmt.enable = true;
           programs.cabal-fmt.enable = true;
           programs.hlint.enable = true;
 
-          # We use fourmolu
-          programs.ormolu.package = pkgs.haskellPackages.fourmolu;
-          settings.formatter.ormolu = {
-            options = [
-              "--ghc-opt"
-              "-XImportQualifiedPost"
-            ];
-          };
+          programs.fourmolu.package = config.fourmolu.wrapper;
+        };
+
+        fourmolu.settings = {
+          indentation = 2;
+          comma-style = "leading";
+          record-brace-space = true;
+          indent-wheres = true;
+          import-export-style = "diff-friendly";
+          respectful = true;
+          haddock-style = "multi-line";
+          newlines-between-decls = 1;
+          extensions = [ "ImportQualifiedPost" ];
         };
 
         # Default package & app.
