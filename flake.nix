ee573c40e5885bd1da8ede2420e9f5bb53f3c53f
Sridhar Ratnakumar <srid@srid.ca>
2024-02-22 14:35:54 -0500

    nix: add data dump

--- flake.nix
+++ flake.nix
@@ -18,6 +18,18 @@
         inputs.treefmt-nix.flakeModule
         inputs.fourmolu-nix.flakeModule
       ];
+      flake.data =
+        let
+          inherit (inputs.nixpkgs) lib;
+          perSystem = k: lib.concatLists (lib.flip lib.mapAttrsToList inputs.self.${k} (system: s:
+            builtins.map (name: "${k}.${system}.${name}") (lib.attrNames s)
+          ));
+        in
+        lib.concatLists [
+          (perSystem "packages")
+          (perSystem "devShells")
+          (perSystem "checks")
+        ];
       perSystem = { self', system, lib, config, pkgs, ... }: {
         # Our only Haskell project. You can have multiple projects, but this template
         # has only one.
