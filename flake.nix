c8372e359c81d5a3cbccf969bb096b555ef88811
Sridhar Ratnakumar <srid@srid.ca>
2024-02-22 15:38:49 -0500

    flat

--- flake.nix
+++ flake.nix
@@ -22,16 +22,14 @@
         let
           inherit (inputs.nixpkgs) lib;
           perSystem = k: lib.concatLists (lib.flip lib.mapAttrsToList inputs.self.${k} (system: s:
-            builtins.map (name: { out = "${k}.${system}.${name}"; }) (lib.attrNames s)
+            builtins.map (name: "${k}.${system}.${name}") (lib.attrNames s)
           ));
         in
-        {
-          include = lib.concatLists [
-            (perSystem "packages")
-            #(perSystem "devShells")
-            #(perSystem "checks")
-          ];
-        };
+        lib.concatLists [
+          (perSystem "packages")
+          #(perSystem "devShells")
+          #(perSystem "checks")
+        ];
       perSystem = { self', system, lib, config, pkgs, ... }: {
         # Our only Haskell project. You can have multiple projects, but this template
         # has only one.
