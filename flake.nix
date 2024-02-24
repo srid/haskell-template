74bf421c2d7d2f32e1c5d175c63ce12f55e1e519
Sridhar Ratnakumar <srid@srid.ca>
2024-02-24 15:48:48 -0500

    devshell: add cabal

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
           defaults.devShell.tools = _:
             let devtools = inputs.horizon-devtools.legacyPackages.${system};
             in {
-              inherit (devtools) ghcid haskell-language-server;
+              inherit (devtools) cabal-install ghcid haskell-language-server;
             };
 
           # Packages to add on top of `basePackages`
