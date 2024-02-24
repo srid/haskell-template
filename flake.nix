83160c9f89105fb43dd1030bd3718bc820c8bac2
Daniel Firth <dan.firth@homotopic.tech>
2024-02-24 20:37:58 +0000

    Add horizon cache

--- flake.nix
+++ flake.nix
@@ -1,5 +1,11 @@
 {
   description = "srid/haskell-template: Nix template for Haskell projects";
+
+  nixConfig = {
+    extra-substituters = "https://horizon.cachix.org";
+    extra-trusted-public-keys = "horizon.cachix.org-1:MeEEDRhRZTgv/FFGCv3479/dmJDfJ82G6kfUDxMSAw0=";
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     horizon-devtools.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-devtools?ref=lts/ghc-9.6.x";
