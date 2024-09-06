7fca847acc06d4be946e56a18eeca767cc43afc7
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-09-05 21:48:38 -0400

    template: add welcomeTet

--- nix/modules/template.nix
+++ nix/modules/template.nix
@@ -4,5 +4,8 @@
   flake.templates.default = {
     description = "A batteries-included Haskell project template for Nix";
     path = builtins.path { path = inputs.self; };
+    welcomeText = ''
+      Please see https://srid.ca/haskell-template/start
+    '';
   };
 }
