5abfbedd090d37d896055a531b99264fe009fa51
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-09-19 15:00:56 -0400

    nix: Add `om init` template spec

--- nix/modules/template.nix
+++ nix/modules/template.nix
@@ -1,11 +1,48 @@
 { inputs, ... }:
 
 {
-  flake.templates.default = {
-    description = "A batteries-included Haskell project template for Nix";
-    path = builtins.path { path = inputs.self; };
-    welcomeText = ''
-      Please see https://srid.ca/haskell-template/start
-    '';
+  flake = rec {
+    templates.default = {
+      description = "A batteries-included Haskell project template for Nix";
+      path = builtins.path { path = inputs.self; };
+      welcomeText = ''
+        Please see https://srid.ca/haskell-template/start
+      '';
+    };
+
+    # https://omnix.page/om/init.html#spec
+    om.templates.haskell-template = {
+      template = templates.default;
+      params = [
+        {
+          name = "author";
+          description = "Author name";
+          placeholder = "Sridhar Ratnakumar";
+        }
+        {
+          name = "package-name";
+          description = "Name of the Haskell package";
+          placeholder = "haskell-template";
+        }
+        {
+          name = "vscode";
+          description = "Include the VSCode settings folder (./.vscode)";
+          paths = [ ".vscode" ];
+          value = true;
+        }
+        {
+          name = "github-ci";
+          description = "Include GitHub Actions workflow configuration";
+          paths = [ ".github" ];
+          value = true;
+        }
+        {
+          name = "nix-template";
+          description = "Keep the flake template in the project";
+          paths = [ "**/template.nix" ];
+          value = false;
+        }
+      ];
+    };
   };
 }
