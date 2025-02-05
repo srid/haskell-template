aaad9c2ee8abdd48b255527455c7760096dbab6f
Sridhar Ratnakumar <srid@srid.ca>
2025-02-05 10:49:10 -0500

    nixos-unified: Update

--- flake.nix
+++ flake.nix
@@ -13,10 +13,10 @@
   };
 
   outputs = inputs:
-    # This will import ./nix/modules/flake-parts/*.nix
+    # This will import ./nix/modules/flake/*.nix
     # cf. https://nixos-unified.org/autowiring.html#flake-parts
     #
-    # To write your own Nix, add or edit files in ./nix/modules/flake-parts/
+    # To write your own Nix, add or edit files in ./nix/modules/flake/
     inputs.nixos-unified.lib.mkFlake
       { inherit inputs; root = ./.; };
 }
